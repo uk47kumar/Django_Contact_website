d6bdacb215ba9e028a8b13276d0ad30dd768620b
uk47kumar <uk47kumar@gmail.com>
2022-09-12 21:36:07 +0530

    sample commit

---
+++ home/views.py
@@ -0,0 +1,40 @@
+from http.client import HTTPResponse
+from multiprocessing import context
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        "variable": "This is sent by view"
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("this is homepage")
+
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse("this is about page")
+
+
+def services(request):
+    return render(request, 'services.html')
+    # return HttpResponse("this is services page")
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')    
+        email = request.POST.get('email')    
+        phone = request.POST.get('phone')    
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())    
+        contact.save()
+        messages.success(request, 'your message has been sent!')
+    
+    return render(request, 'contact.html')
+    # return HttpResponse("this is contact page")
